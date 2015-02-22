8d93c25e1eeae77f23f4dced5d71e071780a84d2
MartinFeineis <martin.feineis@yahoo.de>
2015-02-21 22:14:25 -0500

    late night

--- JavaScript/scriptFile1.js
+++ JavaScript/scriptFile1.js
@@ -1,8 +1,29 @@
+pulloverFollower = [{ "firstPulli" :  "Martin"  , "surPulli" : "da Pullova" }]
+concatedPullies = new String()
+
 function sendThePullova(){
-var firstname = document.getElementById("firstPulli").value;
-var surname = document.getElementById("surPulli").value;
-console.log(firstname + "  test  "  +surname);
-PulliMemArray =  "{ firstPulli :\""+ firstname + "\" , surPulli :\"" + surname + "\" }";
-console.log(PulliMemArray);
-jasonsPulli = JSON.stringify(PulliMemArray);
-} 
+firstname = document.getElementById("firstPulli").value;
+surname = document.getElementById("surPulli").value;
+PulliMemArray =  { "firstPulli" :  firstname  , "surPulli" : surname}
+pulloverFollower.push(PulliMemArray)
+console.log(pulloverFollower)
+jasonsPulli = JSON.stringify(localStorage.pulloverFollower + PulliMemArray)
+};
+
+
+function parseJSONpullies (pulloverFollower){
+
+
+partOne = "<table>"
+for (var i =0; i<pulloverFollower.length; i++ ) {
+partTwo = "<tr><td> " + pulloverFollower[i].firstPulli.toString() + "</td><td>" +pulloverFollower[i].surPulli.toString()+ "</td>"
+concatedPullies = concatedPullies + partTwo;
+}
+partThree = "</table>"
+wholeString = partOne + concatedPullies + partThree;
+
+console.log(wholeString)
+
+ 
+
+}
